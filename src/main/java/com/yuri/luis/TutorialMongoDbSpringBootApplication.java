2bf3f4442959da3b028db3b129524d359d9f4369
YuriLuisHB022 <yuri.luiz123g@gmail.com>
2020-08-04 11:17:36 -0300

    Projeto Criado

---
+++ src/main/java/com/yuri/luis/TutorialMongoDbSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.luis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialMongoDbSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialMongoDbSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuri/luis/TutorialMongoDbSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.luis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialMongoDbSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
