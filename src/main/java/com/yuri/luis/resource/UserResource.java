a7ddd424f9d3dd610c119845f636b9e69a80b841
YuriLuisHB022 <yuri.luiz123g@gmail.com>
2020-08-04 11:41:52 -0300

    Model Criado e Resorce para usuario com get funcionando

---
+++ src/main/java/com/yuri/luis/resource/UserResource.java
@@ -0,0 +1,26 @@
+package com.yuri.luis.resource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yuri.luis.model.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll() {
+	List<User> list = new ArrayList<>();
+	User maria = new User("1001", "Maria Brown", "maria@gmail.com", "123456");
+	User alex = new User("1002", "Alex Green", "alex@gmail.com", "123456");
+	list.addAll(Arrays.asList(maria, alex));
+	return ResponseEntity.ok().body(list);
+	}
+}
